855f29de21d3800c37d139b079ee3f2935857278
JOSE LEONEL CRUZ LOPEZ <jcruzl20@miumg.edu.gt>
2025-02-14 00:40:35 -0600

    Add files via upload

---
+++ Lab2Parte1/Lab2Parte1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+
+        Console.Write("Ingrese un número entero: ");
+        int entero = int.Parse(Console.ReadLine());
+
+        Console.Write("Ingrese un número decimal: ");
+        double decimalNum = double.Parse(Console.ReadLine());
+
+        Console.Write("Ingrese un texto: ");
+        string texto = Console.ReadLine();
+
+        Console.Write("Ingrese un valor booleano (true/false): ");
+        bool booleano = bool.Parse(Console.ReadLine());
+
+        Console.WriteLine("\nValores ingresados:");
+        Console.WriteLine($"El numero Entero Ingresado es: {entero}");
+        Console.WriteLine($"El numero Decima ingresado esl: {decimalNum}");
+        Console.WriteLine($"El Texto ingresado es: {texto}");
+        Console.WriteLine($"El valor Booleano Ingresado es : {booleano}");
+    }
+}
