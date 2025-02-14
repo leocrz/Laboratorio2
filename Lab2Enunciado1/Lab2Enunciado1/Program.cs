f583030cda081407a49ae76012dda98731a601c1
JOSE LEONEL CRUZ LOPEZ <jcruzl20@miumg.edu.gt>
2025-02-14 00:42:39 -0600

    Add files via upload

---
+++ Lab2Enunciado1/Lab2Enunciado1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Ingresa la edad de la persona: ");
+        int edad = Convert.ToInt32(Console.ReadLine());
+
+        if (edad < 12)
+        {
+            Console.WriteLine("Niño");
+        }
+        else if (edad >= 12 && edad <= 17)
+        {
+            Console.WriteLine("Adolescente");
+        }
+        else if (edad >= 18 && edad <= 59)
+        {
+            Console.WriteLine("Adulto");
+        }
+        else
+        {
+            Console.WriteLine("Adulto mayor");
+        }
+
+        Console.WriteLine("\nPresiona cualquier tecla para salir...");
+        Console.ReadKey();
+    }
+}
+
